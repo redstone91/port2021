cc3844acb7bcddee90b49857efcee7f1269a4688
redstone91 <68971196+redstone91@users.noreply.github.com>
2021-07-18 00:33:14 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+(function ($)
+  { "use strict"
+  
+/* 1. Preloder (готовый код, можно использовать в любом проекте) */
+    $(window).on('load', function () {
+      $('#preloader-active').delay(450).fadeOut('slow');
+      $('body').delay(450).css({
+        'overflow': 'visible'
+      });
+    });
+
+/* 2. Sticky And Scroll UP */
+    $(window).on('scroll', function () {
+      var scroll = $(window).scrollTop();
+      if (scroll < 400) {
+        $(".header-sticky").removeClass("sticky-bar");
+        $('#back-top').fadeOut(500);
+      } else {
+        $(".header-sticky").addClass("sticky-bar");
+        $('#back-top').fadeIn(500);
+      }
+    });
+
+  // Scroll Up
+    $('#back-top a').on("click", function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+
+
+
+  
+
+})(jQuery);
+
+
+const scene = document.getElementById('scene');
+const parallaxInstance = new Parallax(scene);
